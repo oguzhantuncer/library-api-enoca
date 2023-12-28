dbb72340b3ab5596a06be9096a6cfb0b877a882a
Oguzhan Tuncer <oguzhan.tuncer@ogr.altinbas.edu.tr>
2023-12-29 02:12:54 +0300

    project_created

---
+++ src/main/java/com/oguzhantuncer/libraryapienoca/LibraryApiEnocaApplication.java
@@ -0,0 +1,13 @@
+package com.oguzhantuncer.libraryapienoca;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryApiEnocaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibraryApiEnocaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/oguzhantuncer/libraryapienoca/LibraryApiEnocaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oguzhantuncer.libraryapienoca;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryApiEnocaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
